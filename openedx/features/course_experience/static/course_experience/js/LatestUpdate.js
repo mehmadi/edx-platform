a50aaa8d46ee4fafc95256b9c06f1e5558c76e83
Diana Huang <dkh@edx.org>
2017-08-09 11:04:29 -0400

    Add new latest-update fragment and waffle flag.

---
+++ openedx/features/course_experience/static/course_experience/js/LatestUpdate.js
@@ -0,0 +1,15 @@
+/* globals $ */
+import 'jquery.cookie';
+
+export class LatestUpdate {  // eslint-disable-line import/prefer-default-export
+
+  constructor(options) {
+    if ($.cookie('update-message') == 'hide') {
+      $('.update-message').hide();
+    }
+    $('.dismiss-message button').click(() => {
+        $.cookie('update-message', 'hide', { expires: 1 });
+        $('.update-message').hide();
+    });
+  }
+}
